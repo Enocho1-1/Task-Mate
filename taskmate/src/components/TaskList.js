11b26706e3887083fe86effab73b2a6c64aad804
Enoch Okyere <73598581+Enocho1-1@users.noreply.github.com>
2023-04-10 13:03:33 -0400

    5th commit

--- taskmate/src/components/TaskList.js
+++ taskmate/src/components/TaskList.js
@@ -1,12 +1,16 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Form } from "./Form"
 import { Userlist } from "./Userlist"
 import "./tasklist.css"
 
 export const TaskList = () => {
   
-    const [taskList, setTasklist] = useState([])
+    const [taskList, setTasklist] = useState(JSON.parse(localStorage.getItem("taskList")) || [])
     const [taskEdit, setTaskEdit] = useState({})
+
+    useEffect( () => {
+       localStorage.setItem("taskList", JSON.stringify(taskList))
+    },[taskList])
   return (
     <section className="taskContainer">
         <h1>Task Mate App</h1>
