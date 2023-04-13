7d58fe7c9efd3a605ec5dc6e48c05b9165c21fd8
Enoch Okyere <73598581+Enocho1-1@users.noreply.github.com>
2023-04-13 11:49:09 -0400

    6th commit

--- taskmate/src/components/Userlist.js
+++ taskmate/src/components/Userlist.js
@@ -19,6 +19,7 @@ export const Userlist = ({tasks, setTasks,  taskEdit, setTaskEdit}) => {
             <span>
                 <p>{tasks.length}</p>
             </span>
+            <button className="clear" onClick={() => {setTasks([])}}>CLEAR</button>
         </div>
         <hr />
         <ul>
